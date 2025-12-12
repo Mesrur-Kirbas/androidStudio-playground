26063e1adf04e1fa5a4405dd126870d5553cbf7b
Muhammed Mesrur Kırbaş <m.mesrur@hotmail.com>
2025-12-12 18:58:32 +0300

    JavaArtBook

---
+++ JavaArtBook/app/src/main/java/com/example/javaartbook/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.javaartbook;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.javaartbook.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    ArrayList<Art> artArrayList;
+    ArtAdaptor artAdaptor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        artArrayList = new ArrayList<>();
+
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        artAdaptor = new ArtAdaptor(artArrayList);
+        binding.recyclerView.setAdapter(artAdaptor);
+
+        getData();
+
+    }
+
+    public void getData(){
+        try {
+            SQLiteDatabase sqLiteDatabase = this.openOrCreateDatabase("Arts",MODE_PRIVATE,null);
+
+            Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM arts", null);
+            int nameIx = cursor.getColumnIndex("artname");
+            int idIx = cursor.getColumnIndex("id");
+
+            while (cursor.moveToNext()){
+                String name = cursor.getString(nameIx);
+                int id = cursor.getInt(idIx);
+                Art art = new Art(name,id);
+                artArrayList.add(art);
+            }
+            artAdaptor.notifyDataSetChanged();
+            cursor.close();
+
+        }catch (Exception e){
+            Log.e("MainActivity","Veritabanına erişim sırasında hata oluştu",e);
+            Toast.makeText(this,"Veritabanına erişim sırasında hata oluştu",Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.art_menu,menu);
+
+        getSupportActionBar().setTitle("Java Art Book");
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == R.id.add_art){
+            Intent intent = new Intent(this,ArtActivity.class);
+            intent.putExtra("info", "new");
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
