3c86c3ad257ac332bf699949b08f07e9946a6a83
Muhammed Mesrur Kırbaş <m.mesrur@hotmail.com>
2026-01-07 03:20:20 +0300

    SignIn&SignUp&SignOut

--- JavaInstagramClone/app/src/main/java/com/example/javainstagramclone/FeedActivity.java
+++ JavaInstagramClone/app/src/main/java/com/example/javainstagramclone/FeedActivity.java
@@ -1,24 +1,56 @@
 package com.example.javainstagramclone;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class FeedActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_feed);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+        auth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.option_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.add_post){
+            //Upload Activity
+            Intent intentUpload = new Intent(FeedActivity.this, UploadActivity.class);
+            startActivity(intentUpload);
+        } else if (item.getItemId() == R.id.signout) {
+            // SignOut
+
+            auth.signOut();
+
+            Intent intentToMain = new Intent(FeedActivity.this, MainActivity.class);
+            startActivity(intentToMain);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- JavaInstagramClone/app/src/main/java/com/example/javainstagramclone/MainActivity.java
+++ JavaInstagramClone/app/src/main/java/com/example/javainstagramclone/MainActivity.java
@@ -18,6 +18,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.Firebase;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,10 +33,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(view);
 
         auth = FirebaseAuth.getInstance();
+
+        FirebaseUser user = auth.getCurrentUser();             // KULLANICI ZATEN GIRIS YAPTIYSA TEKRAR PARALO ISTEMEZ
+        if (user != null){
+            Intent intent = new Intent(MainActivity.this,FeedActivity.class);
+            startActivity(intent);
+            finish(); 
+        }
+
     }
 
 
     public void signInClick(View view){
+    String email = binding.emailText.getText().toString();
+    String password = binding.passwordText.getText().toString();
+
+    if (email.equals("") || password.equals("")){
+        Toast.makeText(this, "Enter email and password!!",Toast.LENGTH_LONG).show();
+    }else{
+        auth.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Intent intent = new Intent(MainActivity.this, FeedActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
 
     }
 
