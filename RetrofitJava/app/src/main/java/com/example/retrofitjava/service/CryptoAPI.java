0fdcee264972ef8ae791c44b215744acc4ca734b
Muhammed Mesrur Kırbaş <m.mesrur@hotmail.com>
2026-02-02 17:09:42 +0300

    CreatingModel-addInterface-getData-RecycleViewLayout

---
+++ RetrofitJava/app/src/main/java/com/example/retrofitjava/service/CryptoAPI.java
@@ -0,0 +1,22 @@
+package com.example.retrofitjava.service;
+
+import com.example.retrofitjava.model.CryptoModel;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface CryptoAPI {
+
+    //GET, POST, UPDATE, DELETE
+
+    // URL BASE -> www.website.com
+    // GET -> price?key=xxx
+
+    //https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/refs/heads/master/crypto.json
+
+    @GET("atilsamancioglu/K21-JSONDataSet/refs/heads/master/crypto.json")
+    Call<List<CryptoModel>> getData();
+
+}
---
+++ RetrofitJava/app/src/main/java/com/example/retrofitjava/view/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.retrofitjava.view;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.retrofitjava.R;
+import com.example.retrofitjava.model.CryptoModel;
+import com.example.retrofitjava.service.CryptoAPI;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<CryptoModel> cryptoModels;
+    private String BASE_URL = "https://raw.githubusercontent.com/";
+    Retrofit retrofit;
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/refs/heads/master/crypto.json
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        //Retrofit & JSON
+
+        Gson gson = new GsonBuilder().setLenient().create();
+
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        loadData();
+
+    }
+
+    private void loadData(){
+        CryptoAPI cryptoAPI = retrofit.create(CryptoAPI.class);
+
+        Call<List<CryptoModel>> call = cryptoAPI.getData();
+
+        call.enqueue(new Callback<List<CryptoModel>>() {
+            @Override
+            public void onResponse(Call<List<CryptoModel>> call, Response<List<CryptoModel>> response) {
+                if (response.isSuccessful()){
+                    List<CryptoModel> responseList =  response.body();
+                    cryptoModels = new ArrayList<>(responseList);
+
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<CryptoModel>> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
+    }
+}
